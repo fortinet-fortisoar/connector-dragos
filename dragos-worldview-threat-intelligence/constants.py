c74f065994d05d49a0d19e1f1d36f07fa4698380
cs-rahul-vishwakarma <rvishwakarma@fortinet.com>
2022-12-23 15:44:01 +0530

    Major Changes

--- dragos-worldview-threat-intelligence/constants.py
+++ dragos-worldview-threat-intelligence/constants.py
@@ -16,15 +16,15 @@ PARAM_MAPPING = {
     "Threat": "threat",
     "TLP": "tlp",
     "Release Date": "release_date",
-    "ASC": "true",
-    "DESC": "false"
-
+    "ASC": "false",
+    "DESC": "true"
 }
+
 ENDPOINT_MAPPING = {
-    "Product Metadata": 'products/{id}',
-    "CSV File": 'products/{id}/csv',
+    "Save as CSV": 'products/{id}/csv',
     "STIX2.0 JSON": 'products/{id}/stix2',
 }
+
 encode_keys = {'tags': 'tags%5B%5D', 'serial': 'serial%5B%5D', 'serials': 'serials%5B%5D'}
 indicator_params = ['page', 'page_size', 'updated_after', 'value', 'type', 'serial', 'tags']
 product_params = ['page', 'page_size', 'sort_by', 'sort_desc', 'updated_after', 'serials', 'indicator']
